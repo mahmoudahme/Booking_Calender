0dc93a9a5052b5bb5630749c596caa154116fcc1
mahmoudahme <mahmoudahme@example.com>
2026-01-20 13:46:39 +0200

    Fix CORS and Private Network Access

--- El_Hosn_App_Backend/src/main.ts
+++ El_Hosn_App_Backend/src/main.ts
@@ -10,11 +10,21 @@ async function bootstrap() {
 
   app.enableCors({
     origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
+    allowedHeaders: 'Content-Type, Accept, Authorization, Access-Control-Allow-Private-Network',
+  });
+
+  // Custom middleware for Private Network Access (PNA)
+  app.use((req, res, next) => {
+    if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Private-Network', 'true');
+    }
+    next();
   });
 
   const port = configService.get('APP_PORT') || 3000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
 
   console.log(`🚀 Application is running on: http://localhost:${port}`);
 }
